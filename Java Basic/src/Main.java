82fd4ab0e18a876d1cc6180e303236e1d0d7cf76
shauravbaramu <sauravmagar2000@gmail.com>
2024-06-25 21:25:04 -0400

    Class Pen created

--- Java Basic/src/Main.java
+++ Java Basic/src/Main.java
@@ -1,6 +1,24 @@
+class Pen {
+    String color;
+    String type;
+
+    public void write(){
+        System.out.println("The specifications of this pen are:");
+        System.out.println(color + " " + type);
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        Pen pen1 = new Pen();
+        Pen pen2 = new Pen();
+
+        pen1.color = "Green";
+        pen1.type = "Dot Pen";
+        pen2.color = "Red";
+        pen2.type = "Gel Pen";
+        pen1.write();
+        pen2.write();
     }
 }
