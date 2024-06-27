825d1645496d55f02ed855ce604c21d59d20e8c3
shauravbaramu <sauravmagar2000@gmail.com>
2024-06-27 14:36:45 -0400

    Java simple GUI

--- Java Basic/src/Practice.java
+++ Java Basic/src/Practice.java
@@ -1,5 +1,14 @@
+import javax.swing.JOptionPane;
+
+
+
 public class Practice {
-    Public static void main(String[] args) {
-        System.out.println("Hello World");
+
+    public static void main(String[] args) {
+        String name = JOptionPane.showInputDialog("What is your name?");
+        JOptionPane.showMessageDialog(null, "Your name is "+ name);
+
+        int age = Integer.parseInt(JOptionPane.showInputDialog("What is your age?"));
+        JOptionPane.showMessageDialog(null, "Your age is "+ age);
     }
 }
